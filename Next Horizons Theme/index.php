43c69619d65d48e538e615fbf4a5e943cf61c3d7
Artemis Levane <art@artlevane.com>
2022-05-22 23:16:31 -0400

    Custom Wordpress theme built with HTML5Blank

---
+++ Next Horizons Theme/index.php
@@ -0,0 +1,254 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title>Next Horizons Code Test</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <!-- End Bootstrap CSS -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body style="margin-top: 80px;">
+
+  <!-- NavBar-->
+
+  <ul class="nav nav-pills nav-fill container" style="padding-bottom:15px;">
+    <li class="nav-item">
+      <a class="nav-link" href="#" style="color: black;">Logo</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 1</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 2</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link">Link 3</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link">Link 4</a>
+    </li>
+  </ul>
+
+  <!-- End NavBar -->
+  
+  <main>
+  
+    <section class="py-5 text-align-left container-fluid" style="background-image: url('https://images.unsplash.com/photo-1477884213360-7e9d7dcc1e48?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870'); background-size: cover;">
+      <div class="row py-lg-5 container" style="margin: auto;">
+        <div class="col-lg-6 col-md-8 mx-auto" style="color:white; padding-right: 130px; padding-top: 70px;">
+          <h1 class="fw-bold">Hi Harry!</h1>
+          <p class="lead">This was a fun test. To see the code for this static page <a href="https://github.com/artlevane/NH-CodeTest">click here</a>. To see the dynamic, custom-Wordpress-theme version <a href="https://nh.artlevane.com">click here</a>.</p>
+        </div>
+        <form class="col-lg-6 col-md-8 mx-auto card" style="padding:80px 40px;">
+          <h4>Form</h4>
+          <div class="mb-3">
+            <input type="email" class="form-control" id="exampleInputName" placeholder="Name">
+          </div>
+          <div class="mb-3">
+            <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Email">
+          </div>
+          <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Receive our monthly newsletter</label>
+          </div>
+          <button type="submit" class="btn btn-primary">Sign Up</button>
+          <hr style="border-top: 3px solid black; margin:20px 0px;">
+          <div id="emailHelp" class="form-text">By clicking sign up, you agree to the terms of use.</div>
+        </form>
+      </div>
+    </section>
+
+  
+    <div class="py-5 bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-4">
+            <div class="card shadow-sm">
+              <h4 class="text-center">Column 1</h4>
+              <svg class="bd-placeholder-img card-img-top" style="background-image: url('https://images.unsplash.com/photo-1626211596179-d1fe8beaf75c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=409'); background-size:cover;" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"></svg>
+  
+              <div class="card-body">
+                <h5>H5 Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="submit" class="btn btn-primary">Go somewhere</button>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card shadow-sm">
+              <h4 class="text-center">Column 2</h4>
+              <svg class="bd-placeholder-img card-img-top" style="background-image: url('https://images.unsplash.com/photo-1626211596179-d1fe8beaf75c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=409'); background-size:cover;" style="background-image: url('https://images.unsplash.com/photo-1626211596179-d1fe8beaf75c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=409'); background-size:cover;" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"></svg>
+  
+              <div class="card-body">
+                <h5>H5 Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="submit" class="btn btn-primary">Go somewhere</button>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card shadow-sm">
+              <h4 class="text-center">Column 3</h4>
+              <svg class="bd-placeholder-img card-img-top" style="background-image:url('https://images.unsplash.com/photo-1626211596179-d1fe8beaf75c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=409'); background-size:cover;" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"></svg>
+  
+              <div class="card-body">
+                <h5>H5 Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="submit" class="btn btn-primary">Go somewhere</button>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="card container" style="margin-top: 40px;">
+            <div class="row">
+            <div class="col-6">
+              <div class="shadow-sm">
+                <svg class="bd-placeholder-img card-img-top" style="background-image:url('https://images.unsplash.com/photo-1626211596179-d1fe8beaf75c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=409'); background-size:cover;" width="100%" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"></svg>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="shadow-sm">
+                <div class="card-body">
+                  <h2>H2 HEADER</h2>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+          <h2 style="margin-top:40px;">H2 HEADER</h2>
+          <div class="container">
+            <div class="row">
+              <div class="col-sm">
+               <div class="container card">
+                 <div class="row">
+                   <div class="col-6">
+                     <div class="card-body lab">
+                      <p class="font-weight-bold">Bold Text</p>
+                      <h3>H3 HEADER</h3>
+                      <p class="text-muted">Nov 12</p>
+                      <p>Lorem ipsum</p>
+                      <a href="#">
+                        Continue reading
+                      </a>
+                    </div>
+                   </div>
+                   <div class="col-6" style="height: 200px; background-image: url('https://images.unsplash.com/photo-1552053831-71594a27632d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=362'); background-size: cover; background-position: 20px -60px; background-repeat: no-repeat;">
+                   </div>
+                 </div>
+               </div>
+              </div>
+              <div class="col-sm">
+               <div class="container card">
+                 <div class="row">
+                   <div class="col-6">
+                     <div class="card-body lab">
+                      <p class="font-weight-bold">Design</p>
+                      <h3>H3 HEADER</h3>
+                      <p class="text-muted">Nov 12</p>
+                      <p>Lorem ipsum</p>
+                      <a href="#">
+                        Continue reading
+                      </a>
+                    </div>
+                   </div>
+                   <div class="col-6" style="height: 200px; background-image: url('https://images.unsplash.com/photo-1552053831-71594a27632d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=362'); background-size: cover;  background-position: 20px -60px;  background-repeat: no-repeat;">
+                   </div>
+                 </div>
+               </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  </main>
+  
+  <style>
+    .lab > * {
+      margin: 0;
+    }
+    @media (min-width: 1400px) {
+      .container {
+        max-width: 1120px;
+      }
+    }
+	    .col-6 {
+	padding: 0 !important;
+  }
+  
+  .card-body.lab {
+	padding: 13% 20px !important;
+  }
+  
+  @media only screen and (max-width: 767px) {
+	.col-4 {
+	  width: 100%;
+	  margin-bottom: 30px;
+	}
+	.col-sm {
+	  flex: auto;
+	  margin-top: 20px;
+	}
+  }
+  
+  </style>
+
+
+  <!-- JavaScript -->
+
+  <!-- Bootstrap JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  <!-- End Bootstrap JavaScript Bundle with Popper -->
+  <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- End JavaScript -->
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+
+</html>
